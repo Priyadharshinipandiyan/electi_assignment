b45b24e333f04446e5caf95fb0ba00e6fa1c2487
Priyadharshini_P <priyadharshinipandiyan2002@gmail.com>
2023-02-06 21:31:19 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/electi_assignment/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.electi_assignment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:electi_assignment/screen/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key?key}):super(key: key);
+  @override
+  
+  Widget build(BuildContext context){
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
